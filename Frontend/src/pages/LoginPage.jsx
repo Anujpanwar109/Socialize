8759a9e8c7f22a977a46b4d12af0a5897f5ff56c
Anujpanwar109 <anujpanwar1601@gmail.com>
2025-06-09 15:35:22 +0530

    update

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
-import Creative from '../components/layout/Creative'
+import Creative from '../components/layout/Creative.jsx'
 
 const LoginPage = () => {
     const[email,setEmail]=useState('')
